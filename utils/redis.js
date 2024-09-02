8b3b285d5f8a7f53c4b4f3280c86f1d9299a0962
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-02 17:16:43 +0100

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -11,42 +11,42 @@ class RedisClient {
   isAlive() {
     // Check if the Redis client is connected
     return this.client.connected;
-};
+  }
 
-async get(key) {
+  async get(key) {
     // Asynchronous function to get a value for a given key
     return new Promise((resolve, reject) => {
-        this.client.get(key, (err, value) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(value);
-            }
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
         });
     });
   }
   async set(key, value, duration) {
     // Asynchronous function to set a value with an expiration duration
     return new Promise((resolve, reject) => {
-        this.client.setex(key, duration, value, (err, reply) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(reply);
-            }
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
         });
     });
   }
   async del(key){
     // Asynchronous function to delete a value for a given key
     return new Promise((resolve, reject) => {
-        this.client.del(key, (err, reply) => {
-            if (err) {
+      this.client.del(key, (err, reply) => {
+        if (err) {
                 reject(err);
-            } else {
+        } else {
                 resolve(reply);
-            }
-        });
+        }
+      });
     });
   }
 }
